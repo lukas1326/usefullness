9432799fe6594b58a076071b2cf2bcb7c96c43c0
Olga Lukasevych <olga.lukasevich@gmail.com>
2021-06-22 12:05:12 +0300

    Update date_convert.py

--- date_convert.py
+++ date_convert.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[6]:
-
+# columns date format like 2017-10-15 12:30:46,000
 
 import pandas as pd
-# парсер даты
+
 def parse_date(x):
     return pd.to_datetime(str(x)[:10],format = '%Y-%m-%d')
-# определить колонки с датами
 
-#date_columns = ['StartDate','PlanDate']
 def convert_columns(date_columns, df):
     for i in date_columns:
         df[i] = df[i].apply(lambda x:parse_date(x))
     return df
-    
+
 if __name__ == '__main__':
     file = input('Введіть назву файлу: ')
     df = pd.read_csv(file,delimiter=';',parse_dates=False)
@@ -23,12 +20,3 @@ if __name__ == '__main__':
     date_columns = input('Введіть назви колонок з датами для конвертації: ').split(',')
     result= convert_columns(date_columns, df)
     print('Результат збережено в датафрейм result')
-    
-    
-
-
-# In[ ]:
-
-
-
-
